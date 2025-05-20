ef5fe75ae9a996a4ba54587c577cf39e6b10a5d5
minhthang1204 <>
2025-05-20 10:19:54 +0700

    change code

--- database/index.ts
+++ database/index.ts
@@ -1,15 +1,38 @@
+// import { connect, ConnectOptions } from "mongoose";
+
+// const dbConnection = async () => {
+//   try {
+//     await connect(process.env.MONGO_URI!, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     } as ConnectOptions);
+//     console.log("Mongo successfully connected");
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// export default dbConnection;
+
 import { connect, ConnectOptions } from "mongoose";
 
-const dbConnection = async () => {
+const dbConnection = async (): Promise<void> => {
   try {
-    await connect(process.env.MONGO_URI!, {
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    await connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as ConnectOptions);
-    console.log("Mongo successfully connected");
+
+    console.log("MongoDB successfully connected");
   } catch (error) {
-    console.log(error)
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with failure
   }
-}
+};
 
 export default dbConnection;
